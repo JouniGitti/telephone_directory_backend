47282d521d99dde0c3ee38d68615db2ee9d455c2
jounikoo <jouni.kullervo.kari@gmail.com>
2020-07-14 16:47:58 +0300

    puhelinluettelo back-end 2. commit, mukana optimoitu build

---
+++ build/precache-manifest.6912379b379ca7c76de60863bb4825e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e23de779cf2497d82f615e3c725cc4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a812be56a41b633b702",
+    "url": "/static/css/main.091b80ba.chunk.css"
+  },
+  {
+    "revision": "f5f05b176c0c664a379c",
+    "url": "/static/js/2.817d85f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.817d85f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a812be56a41b633b702",
+    "url": "/static/js/main.9a335a7d.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
---
+++ build/precache-manifest.c2efe27bf1c40e49db83f777bc6eb374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab49d17e4bdad1c6889425442e1fb4e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f6bd47477fc49f4b04",
+    "url": "/static/css/main.091b80ba.chunk.css"
+  },
+  {
+    "revision": "f5f05b176c0c664a379c",
+    "url": "/static/js/2.817d85f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.817d85f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f6bd47477fc49f4b04",
+    "url": "/static/js/main.420f84d7.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
